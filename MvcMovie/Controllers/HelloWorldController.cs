1a1d81d116b889e7cfe2553fb8c8df8ebe32accf
Gerardo Monsiváis <gerazmonsivais@gmail.com>
2024-07-01 15:34:37 -0600

    Agregar archivos de proyecto.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+
+//Clase HelloWorld que hereda de Controller esto hace que esta clase actue como Controlador MVC
+public class HelloWorldController : Controller
+{
+    //Metodo de accion Index 
+    public IActionResult Index()
+    {
+        return View();  //Llama al metodo View del cntrolador
+    }
+    // 
+    // GET: /HelloWorld/Welcome/ 
+    public string Welcome(string name, int ID = 1)
+    {
+        //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}"); //Encoder es para proteger la cadena de entradas malintencionadas por ejemplo JavaScript 
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+    }
+}
