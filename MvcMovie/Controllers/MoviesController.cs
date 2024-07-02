b34d60f7729057902c9ac2a18c568228dbb2f751
Gerardo Monsiváis <gerazmonsivais@gmail.com>
2024-07-02 14:18:58 -0600

    Acciones y vistas del controlador.

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -14,6 +14,7 @@ namespace MvcMovie.Controllers
     {
         private readonly MvcMovieContext _context;
 
+        //Este constructor usa la inyeccion de dependencias para insertar el contexto de la BD en el controlador 
         public MoviesController(MvcMovieContext context)
         {
             _context = context;
@@ -26,20 +27,22 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies/Details/5
+        // Le pasaos el id de la pelicula mediante el URL al seleccionarlo desde el navegador
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
+            if (id == null) //Verificamos que no sea nulo el ID
             {
                 return NotFound();
             }
 
-            var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (movie == null)
+            var movie = await _context.Movie //Seleccionamos las entidades que coincidan con los valores de consulta de la cadena
+                .FirstOrDefaultAsync(m => m.Id == id); //Todo lo que coincida con el id
+            if (movie == null) //Si es nulo el id
             {
                 return NotFound();
             }
 
+            //caso contrario, si se encuentra una pelicula pasamos una instancia del modelo movie a la vista de detalles
             return View(movie);
         }
 
@@ -65,93 +68,99 @@ namespace MvcMovie.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+// GET: Movies/Edit/5
+/*
+ * toma el parámetro ID de la película, busca la película con el método FindAsync de Entity Framework y 
+ * devuelve la película seleccionada a la vista de edición. Si no se encuentra una película, se devuelve 
+ * NotFound (HTTP 404).
+ */
 
-            var movie = await _context.Movie.FindAsync(id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-            return View(movie);
-        }
-
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
-        {
-            if (id != movie.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(movie);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MovieExists(movie.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(movie);
-        }
-
-        // GET: Movies/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (movie == null)
-            {
-                return NotFound();
-            }
-
-            return View(movie);
-        }
-
-        // POST: Movies/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var movie = await _context.Movie.FindAsync(id);
-            if (movie != null)
-            {
-                _context.Movie.Remove(movie);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool MovieExists(int id)
-        {
-            return _context.Movie.Any(e => e.Id == id);
-        }
+public async Task<IActionResult> Edit(int? id)
+{
+    if (id == null)
+    {
+        return NotFound();
     }
+
+    var movie = await _context.Movie.FindAsync(id);
+    if (movie == null)
+    {
+        return NotFound();
+    }
+    return View(movie);
+}
+
+// POST: Movies/Edit/5
+// To protect from overposting attacks, enable the specific properties you want to bind to.
+
+[HttpPost]
+[ValidateAntiForgeryToken]
+public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+{
+    if (id != movie.Id)
+    {
+        return NotFound();
+    }
+
+    if (ModelState.IsValid)  //comprueba que los datos enviados en el formulario pueden usarse para modificar
+            {
+        try
+        {
+            _context.Update(movie);  //Si los datos son validos actualizamos
+            await _context.SaveChangesAsync();  //Guardamos los cambios en el contexto 
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!MovieExists(movie.Id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+        return RedirectToAction(nameof(Index));  //una vez que se guardaron los datos redirigimos al Index
+    }
+    return View(movie);
+}
+
+// GET: Movies/Delete/5
+public async Task<IActionResult> Delete(int? id)
+{
+    if (id == null)
+    {
+        return NotFound();
+    }
+
+    var movie = await _context.Movie
+        .FirstOrDefaultAsync(m => m.Id == id);
+    if (movie == null)
+    {
+        return NotFound();
+    }
+
+    return View(movie);
+}
+
+// POST: Movies/Delete/5
+[HttpPost, ActionName("Delete")]
+[ValidateAntiForgeryToken]
+public async Task<IActionResult> DeleteConfirmed(int id)
+{
+    var movie = await _context.Movie.FindAsync(id);
+    if (movie != null)
+    {
+        _context.Movie.Remove(movie);
+    }
+
+    await _context.SaveChangesAsync();
+    return RedirectToAction(nameof(Index));
+}
+
+private bool MovieExists(int id)
+{
+    return _context.Movie.Any(e => e.Id == id);
+}
+}
 }
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,8 +3,9 @@
 //En las clases de modelo de datos solo definen las propiedades de los datos que se almacenan en la base de datos.
 
 using System.ComponentModel.DataAnnotations;
-
 namespace MvcMovie.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 //Clase Movie que tiene los datos referentes a una pelicula
@@ -13,8 +14,10 @@ public class Movie
 {
     public int Id { get; set; }  //Id que sera la llave primaria
     public string? Title { get; set; }  //Titulo de la pelicula, tiene signo de interrogacion por que puede admitir un valor null
+    [Display(Name = "Release Date")] //Agregamos la etiqueta como debe ser y no con el nombre de la variable
     [DataType(DataType.Date)]  //Atributo que que especifica el tipo de datos que sera Date
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
